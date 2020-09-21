45d34d1482e7cf3fa5ca43c4c647444850ec8fb2
TanLong2001 <71588114+TanLong2001@users.noreply.github.com>
2020-09-21 23:40:42 +0700

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
-
 public class Main {
+    public static void main(String[] args) {
+        Dictionary dictionary = new Dictionary();
+        DictionaryManagement.insertFromCommandline(dictionary);
+        DictionaryCommandline.dictionaryBasic(dictionary);
+    }
 }
